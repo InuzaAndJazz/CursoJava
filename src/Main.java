84d3aa491da2a34707b48ed9f5151fdf1165e911
Juan Pablo Villanueva <juan.villanueva@ionix.cl>
2022-05-03 20:56:11 -0400

    Hola Mundo + Declaración de Variables

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		String palabra = "Hola mundo";
+		int entero = -100;
+		double decimal = -1.6;
+		boolean binario = true; // false
+
+		// *Tipo de variable* *nombre de la variable* = *valor*;
+
+		System.out.println("La variable [palabra] es igual a: [" + palabra + "]");
+		System.out.println("La variable [entero] es igual a: [" + entero + "]");
+		System.out.println("La variable [decimal] es igual a: [" + decimal + "]");
+		System.out.println("La variable [binario] es igual a: [" + binario + "]");
+	}
+
+}
