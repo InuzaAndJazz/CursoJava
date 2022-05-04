60a878cd956b7c31e22f7a0f994c6d8c87d198ac
Juan Pablo Villanueva <juan.villanueva@ionix.cl>
2022-05-03 21:16:10 -0400

    calculadora de area con ingreso de texto

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		double largo = 2;
-		double ancho = 3;
+		Scanner lector = new Scanner(System.in);
+		
+		System.out.println("Ingresa el largo del rectangulo: ");
+		String lineaLeida = lector.nextLine();
+		double largo = Double.parseDouble(lineaLeida);
+		
+		System.out.println("Ingresa el ancho del rectangulo: ");
+		lineaLeida = lector.nextLine();
+		double ancho = Double.parseDouble(lineaLeida);
 
 		System.out.println("El area del rectangulo es " + (largo * ancho));
 		System.out.println("El perimetro del rectangulo es " + (largo*2 + ancho*2));
