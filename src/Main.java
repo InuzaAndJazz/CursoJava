e977c9aecf298d2c942ba51dedb6321a16222027
Juan Pablo Villanueva <juan.villanueva@ionix.cl>
2022-05-03 21:00:27 -0400

    Calculadora de año de nacimiento

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,10 @@
 public class Main {
 
 	public static void main(String[] args) {
-		String palabra = "Hola mundo";
-		int entero = -100;
-		double decimal = -1.6;
-		boolean binario = true; // false
+		int edadActual = 28;
+		int fechaActual = 2022;
 
-		// *Tipo de variable* *nombre de la variable* = *valor*;
-
-		System.out.println("La variable [palabra] es igual a: [" + palabra + "]");
-		System.out.println("La variable [entero] es igual a: [" + entero + "]");
-		System.out.println("La variable [decimal] es igual a: [" + decimal + "]");
-		System.out.println("La variable [binario] es igual a: [" + binario + "]");
+		System.out.println("Tú naciste en " + (fechaActual - edadActual));
 	}
 
 }
