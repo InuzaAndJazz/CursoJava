00aefe68412aa796d62805ed0881f8e3ac459348
Juan Pablo Villanueva <juan.villanueva@ionix.cl>
2022-05-31 20:21:01 -0400

    Calculadora de números primos

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 
-		// while y for
-		/*int a = 0;
-		while (a < 10) {
-			System.out.println("a es igual a: " + a);
-			a++; // es igual a escribir [a = a + 1;]
-		}
-		*/
-		
-		//for (declaración de variable ; condicion ; sentencia al final de cada loop)
+		Scanner lector = new Scanner(System.in);
+		System.out.println("Escribe un número: ");
+		int numero_usuario = lector.nextInt();
+		System.out.println("Los números primos encontrados de 1 a " + numero_usuario + " son:");
 
-		for (int a = 0; a < 10; a++) {
-			System.out.println("a es igual a: " + a);
+		for (int i = 1; i <= numero_usuario; i++) {
+//			System.out.println("===================================================");
+//			System.out.println("Validando si el número " + i + " es un número primo");
+//			System.out.println("===================================================");
+			int divisores = 0;
+			for (int j = 1; j <= i; j++) {
+//				System.out.println("Validando si el número " + j + " es un divisor de " + i);
+				if (i % j == 0) {
+					divisores++;
+				}
+			}
+			if (divisores <= 2) {
+				System.out.println(i);
+			} else {
+//				System.out.println(i + " NO es un número primo");
+			}
+
 		}
-		System.out.println("El ciclo terminó");
 	}
 
 }
